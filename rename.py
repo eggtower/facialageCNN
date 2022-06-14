abd4af137d6e5bf40cc9d66053ec6a1481cbf3ab
qq21629489 <o21629489o@gmail.com>
2022-06-14 10:20:50 +0800

    no message

--- rename.py
+++ rename.py
@@ -1,9 +1,14 @@
 import os
 from glob import glob
 
-dir_ = glob('Kaggleface/face/*')
-print(dir_)
+dir_ = glob('Kaggleface\\face\\*')
+# print(dir_)
 
-for i in dir_[:10]:
-    sub = glob('{}/*.png')
-    print(sub)
+for i in dir_:
+    sub = glob('{}\\*.png'.format(i))
+    
+    for j in sub:
+        age = int(j.split('\\')[-2])
+        name = j.split('\\')[-1]
+        out_name = 'Kaggleface\\face\\{}_{}'.format(age, name)
+        os.rename(j, out_name)
